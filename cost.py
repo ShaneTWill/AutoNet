a22e18eb816956ca75e55b3b2c84e60504e9512a
ShaneTWill <ShaneTWill@users.noreply.github.com>
2017-05-21 14:00:36 -0500

    Refactored cost.py to remove duplicated code.

--- cost.py
+++ cost.py
@@ -5,13 +5,30 @@ import os
 
 
 # Dictionary of possible cost functions.
-met = {
-        0 : 'RMSE: Root Mean Squared Error',
-        1 : 'MSE: Mean Squared Error',
-        2 : 'MAE: Mean Absolute Error',
-        3 : 'MAPE: Mean Absolute Percent Error'
-        }
+metrics = {
+           0 : 'RMSE: Root Mean Squared Error'
+           ,1 : 'MSE: Mean Squared Error'
+           ,2 : 'MAE: Mean Absolute Error'
+           ,3 : 'MAPE: Mean Absolute Percent Error'
+          }
 
+# Dictionary of conditional boolean statements used. 
+conditions = {
+              0: (lambda i: i.isdigit() and (-1<int(i)<len(metrics)))
+              ,1: (lambda i: i.isdigit() and (int(i)>0))
+             }
+
+# Dictionary of error message strings.
+errors = {
+          0: 'Sorry that was not an integer, or an option, please try again.'
+          ,1: 'Sorry was not an integer.'
+         }
+
+# Dictionary of prompt message strings.
+prompts = {
+           0: 'What metric from the list below do you want to optimize by?'
+           ,1: 'Finally how many epochs should the model be trained over?'
+          }
 
 def getMetric():
   """
@@ -30,66 +47,39 @@ def getMetric():
   ary = '['
   e = ']'
   
-  ary = ChooseMetric(ary)
-  ary = getEpoch(ary)
-  ary = ary + e
+  ary = getData(0,ary)
+  ary = getData(1,ary)
+  ary += e
 
   return ary
 
 
-def ChooseMetric(ary):
+def getData(code,ary):
   """
-  Asks the user to choose a cost function to optimize by, and returns the value chosen. 
+    Gets the number of epochs to train over from the user and returns the modified 
+    string ary.
 
-  Keyword arguments:
-  ary --  A string that the metric is concatenated on to.
+    Keyword arguments:
+    ary -- A string that the number of epochs is concatenated on to.
 
-  Returns:
-  ary -- The inputted string with the metric concatenated onto it.
+    Returns:
+    ary -- The inputted string with number of epochs concatenated onto it.
   """
   
-  print('What metric from the list below do you want to optimize by?')
-  for k in met.keys():
-    print(str(k)+": "+str(met[k]))
+  print(prompts[code])
+  if(code == 0):
+    for key, value in metrics.items():
+      print('{}: {}'.format(key,value))
 
   while True:
     i = sys.stdin.readline().strip()
-    if(i.isdigit() and (-1<int(i)<4)):
+    if(conditions[code](i)):
       ary = ary + str(i) + ','
       break
-    
+                                          
     else:
-      print('Sorry that was not an integer, or an option, please try again.')
-    i = sys.stdin.readline().strip()
-  os.system('cls' if os.name == 'nt' else 'clear')
-      
-  return ary
-
-
-def getEpoch(ary):
-  """
-  Gets the number of epochs to train over from the user and returns the modified 
-  string ary.
-
-  Keyword arguments:
-  ary -- A string that the number of epochs is concatenated on to.
-
-  Returns:
-  ary -- The inputted string with number of epochs concatenated onto it.
-  """
-
-  print('Finally how many epochs should the model be trained over?')
-  while True:
-    i = sys.stdin.readline().strip()
-    if(i.isdigit() and (int(i)>0)):
-      ary = ary + str(i)
-      break
-  
-    else:
-      print('Sorry was not an integer.')
-    i = sys.stdin.readline().strip()
+      print(errors[code])
   os.system('cls' if os.name == 'nt' else 'clear')
 
   return ary
-
 
